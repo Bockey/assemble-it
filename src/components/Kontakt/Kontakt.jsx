325d352503dcc44a215de0579af5ef1efa04c33b
Bockey <71261529+Bockey@users.noreply.github.com>
2022-10-26 14:02:14 +0200

    maps fixed

--- src/components/Kontakt/Kontakt.jsx
+++ src/components/Kontakt/Kontakt.jsx
@@ -6,27 +6,27 @@ function Kontakt(props) {
   return (
     <div className="contact">
       <div className="container">
-        <Info />
-        <FormEl />
-        <div className="location">
-        <MapContainer
-        center={[59.9377644, 10.8795503]}
-        zoom={12}
-        scrollWheelZoom={true}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[59.9372844, 10.8812603]}>
-          <Popup>
-           
-            Micheletveien 54, 1053 Oslo
-          </Popup>
-        </Marker>
-      </MapContainer>
-      </div>
-    </div>
+            <Info />
+            <FormEl />
+            <div className="location">
+              <MapContainer
+              center={[59.9377644, 10.8795503]}
+              zoom={12}
+              scrollWheelZoom={true}
+            >
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker position={[59.9372844, 10.8812603]}>
+                <Popup>
+                
+                  Micheletveien 54, 1053 Oslo
+                </Popup>
+              </Marker>
+            </MapContainer>
+          </div>
+        </div>
       </div>
       
   );
